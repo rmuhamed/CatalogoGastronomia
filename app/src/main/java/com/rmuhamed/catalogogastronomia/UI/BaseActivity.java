236e3ad22acc9c1b01d85bdd03fb74944967e149
rmuhamed <ricardo.muhamed@gmail.com>
2016-04-03 18:37:54 -0300

    DB Support

--- app/src/main/java/com/rmuhamed/catalogogastronomia/UI/BaseActivity.java
+++ app/src/main/java/com/rmuhamed/catalogogastronomia/UI/BaseActivity.java
@@ -4,6 +4,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -20,19 +21,25 @@ public class BaseActivity extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
 
+        PackageInfo packageInfo = this.getPackageInformation();
+
+        //Database configuration
+        DatabaseConfigurator.getInstance().setContext(this);
+        DatabaseConfigurator.getInstance().configure(BaseUtils.getApplicationVersionCode(packageInfo), null);
+    }
+
+    @Nullable
+    private PackageInfo getPackageInformation() {
         PackageInfo packageInfo = null;
         try {
             packageInfo = this.getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
         } catch (PackageManager.NameNotFoundException e) {
             Log.e(this.getClass().getSimpleName(), "Meta Data of App", e);
         }
-
-        //Configures Database
-        DatabaseConfigurator.getInstance().setContext(this);
-        DatabaseConfigurator.getInstance().configure(BaseUtils.getApplicationVersionCode(packageInfo), null);
+        return packageInfo;
     }
 
     public CatalogoApplication getMyApplication() {
-        return (CatalogoApplication) getApplicationContext();
+        return (CatalogoApplication) this.getApplicationContext();
     }
 }
